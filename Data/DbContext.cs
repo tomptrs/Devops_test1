8a7ea719e3136a9630fce11d9099e02053e6deb2
tom_ptrs@yahoo.com <tom_ptrs@yahoo.com>
2020-02-09 09:51:16 +0100

    first commit

---
+++ Data/DbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data
+{
+    public class MyDbContext:DbContext
+    {        
+        public MyDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Data/ProductRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data
+{
+    public class ProductRepository : IProductRepository
+    {
+        MyDbContext myContext;
+        public ProductRepository(MyDbContext context)
+        {
+            myContext = context;
+        }
+        public void Save(Product product)
+        {
+            myContext.Add(product);
+        }
+
+        public void SaveChanges()
+        {
+            myContext.SaveChanges();
+        }
+    }
+}
