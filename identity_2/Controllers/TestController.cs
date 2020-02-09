9347cdd66eeb976d1417ad0906fb6888d8ca74aa
tom_ptrs@yahoo.com <tom_ptrs@yahoo.com>
2020-02-09 10:09:50 +0100

    user story 1

---
+++ identity_2/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace identity_2.Controllers
+{
+    public class TestController : Controller
+    {
+        /*
+         * This is a test controller!!
+         */
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
